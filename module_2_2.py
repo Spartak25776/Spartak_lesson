eeeeda34d73bffdf649c7a893ac92a6a449d1645
Spartak25776 <spartak25776@gmail.com>
2024-12-22 10:33:13 +0300

    Add files via upload
    Добавлен файл для домашнего задания

---
+++ module_2_2.py
@@ -0,0 +1,16 @@
+# А.Мудрицкий. "Условная конструкция. Операторы if, elif, else"
+
+# Begin
+first = input('Введите первое целое число: ')
+second = input('Введите второе целое число: ')
+third = input('Введите третье целое число: ')
+print ('Отлично, проверяем результат: ')
+if first == second and first == third:
+    print ('Третье число: ', third)
+
+elif first == second or second == third or first == third:
+    print ('Второе число: ', second)
+
+elif first != second and second != third and first != third:
+    print ('Среди введенных чисел равных нет: ', 0)
+# End
